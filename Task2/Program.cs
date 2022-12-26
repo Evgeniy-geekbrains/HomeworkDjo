d54d4beb4dd43fa7df83d254d8d76c854f6cabda
Evgenii <89522778813@mail.ru>
2022-12-26 21:31:16 +0300

    Add Task 2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a>b)
+{
+    WriteLine("Первое число больше второго!");    
+}
+else
+{
+    WriteLine("Второе число больше первого!");
+}
