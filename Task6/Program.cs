a2ed6f420423bfbfc57c4c37c1b4f7375159e693
Evgenii <89522778813@mail.ru>
2022-12-26 21:49:29 +0300

    Add Task 6

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным.
+
+using static System.Console;
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+if (a%2 == 0)
+{
+    WriteLine("Число является чётным!");
+}
+else
+{
+    WriteLine("Число не является чётным!");
+}
